37b08a8b04f7611e2a880677f840731fcca477e0
jihuayu <8042833@qq.com>
2020-03-23 22:46:30 +0800

    up

--- task1.js
+++ task1.js
@@ -1,7 +1,6 @@
 const twitchBase = "https://addons-ecs.forgesvc.net/";
 const axios = require("axios");
 const mongoose = require('mongoose');
-const schedule = require('node-schedule');
 
 
 mongoose.connect(`mongodb+srv://${process.env.USER}@cluster0-7aolm.azure.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`, {useNewUrlParser: true, useUnifiedTopology: true});
